308189f450a642f1168c5bf1dc15c4a958cbf239
swaleh <73616152+salah5495@users.noreply.github.com>
2023-01-29 22:06:50 +0300

    Update App.js

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Home Page />} />
         <Route path="/pos" element={<POSPage />} />
       </Routes>
     </Router>
